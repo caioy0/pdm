391fb05fc49fd70ea8a0dc37ee12cf1c4636f5a5
Caio <caio.yugo@proton.me>
2026-03-02 13:49:12 -0300

    rebase: aula 2

--- react/ap02/src/App.jsx
+++ react/ap02/src/App.jsx
@@ -1,10 +1,11 @@
-const App = () => {
-    return <div style={{margin: 'auto' width:>
-            <p> Hello, React!</p>
-
+const App = () => (
+    <div className="container border mt-2">
+        <div className="row">
+            <div className="col-12">
+                <i className="fa fa-hippo"></i>
+            </div>
         </div>
-}
+    </div>
+)
 
 export default App
-
-//export default function
---
+++ react/ap02/src/old_App.jsx
@@ -0,0 +1,20 @@
+const App = () => (
+     <div className="container border py-5">
+        <div className="row">
+            <div className=" col-sm-12 col-md-3 col--lg-3 p-4 bg-primary">
+
+            </div>
+            <div className="col-sm-12 col-md-9 col-lg-9 p-4 bg-secondary">
+
+            </div>
+
+        </div>
+        <div className="row">
+            
+        </div>
+     </div>
+)
+
+export default App
+
+//export default function
